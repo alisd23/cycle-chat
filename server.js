a12b65f331ed98c01a5ca3b018012b6dc63f55bb
Ali Sheehan-Dare <alisd23@live.co.uk>
2016-04-23 11:52:20 +0100

    Removed all the crap

--- server.js
+++ server.js
@@ -2,17 +2,6 @@ var webpack = require('webpack');
 var WebpackDevServer = require('webpack-dev-server');
 var config = require('./webpack.config');
 
-
-// Start watching and bundling tests here
-var tests = require('./webpack.test.config'),
-  testsCompiler = webpack(tests);
-
-testsCompiler.watch({}, function (err) {
-  if (err) console.log(err);
-  //console.log('Test file bundled');
-});
-
-
 // Primary app
 new WebpackDevServer(webpack(config), {
   publicPath: config.output.publicPath,
--- src/main.js
+++ src/main.js
@@ -1,17 +1,8 @@
-import Rx             from 'rx'
-import navbar         from './dialogue/components/navbar/navbar-index'
-import contentRouter  from './dialogue/components/content-router/content-router-index'
+import Rx from 'rx';
+
 // @cycle/dom has a hyperscript-helper built in so you can
 // declare all html elements you are going to use like div, h1, h2, nav etc
-import {div}    from '@cycle/dom'
-
-// view is what we'd like to display in this case our sidebar + content all wrapped in a div
-const view = (navbar, content) => {
-  return div('#layout .pure-g', [
-    div('.sidebar .pure-u-1 .pure-u-md-1-4', div('.header', [navbar])),
-    div('.content .pure-u-1 .pure-u-md-3-4', [content])
-  ])
-};
+import {h1} from '@cycle/dom';
 
 // we need to pass our components to cycle and give them a "source" when they come from cycle
 // creating this "cycle", here you can see that view$ is a Rx Observable containing out "view"
@@ -19,22 +10,13 @@ const view = (navbar, content) => {
 // variables. We return all of this in an Object with DOM + History
 function main(sources) {
 
-  const Content = contentRouter(sources);
-  const Nav = navbar(sources);
-  const Props = Content.Props;
-
   const view$ = Rx.Observable.just(
-    view(
-      Nav.DOM,
-      Content.DOM
-    )
+    h1('#main', 'This is meant to be a chat app')
   );
 
   return {
-    DOM: view$,
-    History: Nav.url$,
-    Props: Props,
+    DOM: view$
   }
 };
 
-export default main
+export default main;
